43a9c7f9c827cc0b617881c9c91e3dc0f8d4ca9d
Code-JV-export <josevagner9@gmail.com>
2022-01-26 17:07:49 -0300

    first commit

---
+++ src/main/java/com/bootcamps/api/bootcampsplatformapi/BootcampsPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamps.api.bootcampsplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampsPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampsPlatformApiApplication.class, args);
+	}
+
+}
